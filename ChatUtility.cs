8ce25f9101c0e67b48e16b293954955581003495
hskwakr <hskwakr.firefox@gmail.com>
2020-08-21 13:08:30 +0900

    I forgot to remove code from generic class

--- ChatUtility.cs
+++ ChatUtility.cs
@@ -12,45 +12,6 @@ namespace TwitchChatSpeech
     {
         
 
-        public static string Replace(string chat)
-        {
-            Word[] words = Words.Read().ToArray();
-
-            //Word[] words = new Word[]
-            //{
-            //    new Word{ 
-            //        Pattern = @"([8]{4,})", 
-            //        Replace = "ぱちぱち" 
-            //    },
-            //    new Word{ 
-            //        Pattern = @"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", 
-            //        Replace = "url" 
-            //    }
-            //};
-
-            RegexOptions options = RegexOptions.IgnoreCase | RegexOptions.Multiline;
-
-            foreach (var word in Words.Read())
-            {
-                Match match = Regex.Match(chat, word.Pattern, options);
-
-                //Console.WriteLine(match.Success.ToString());
-                if (match.Success)
-                {
-                    // replace
-                    chat = chat.Replace(match.Value, word.Replace);
-                    //Console.WriteLine(chat);
-                }
-            }
-
-            return chat;
-        }
-
-        public static void Addreplace(string pattern, string replace)
-        {
-            Words.Add(pattern, replace);
-        }
-
         public static void Urlfinder(string chat)
         {
             Regex re = new Regex(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.IgnoreCase | RegexOptions.Multiline);
