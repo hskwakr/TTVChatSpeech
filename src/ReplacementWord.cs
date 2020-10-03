d65b0b099c0efc3887b2b4f6edaadc29300b6d3c
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-10-03 18:21:58 +0900

    I deleted wasted using statement

--- src/ReplacementWord.cs
+++ src/ReplacementWord.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using System.IO;
-using TwitchLib.Api.Core.Extensions.System;
 using System.Text.RegularExpressions;
-using System.Runtime.Serialization;
 
 namespace TwitchChatSpeech
 {
